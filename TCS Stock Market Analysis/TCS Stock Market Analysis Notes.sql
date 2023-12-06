d7b7ee706c1390d51b42be5890e25a0aa373aeba
ShabdaliSuchitraSanjayJagtap1214 <140742495+ShabdaliSuchitraSanjayJagtap1214@users.noreply.github.com>
2023-12-06 12:12:41 +0530

    Create TCS Stock Market Analysis Notes.sql

---
+++ TCS Stock Market Analysis/TCS Stock Market Analysis Notes.sql
@@ -0,0 +1,15 @@
+1)Want only Day from the Column:-
+     Formula :- Day = DAY('TCS NS'[Date].[Date])
+
+2)Want only Month from the Column:-
+     Formula :- Month = MONTH('TCS NS'[Date].[Date])
+
+3)Want Quarter from the Column:-
+     Formula :- Quarter = QUARTER('TCS NS'[Date].[Date])
+     Quarter 1 :- Jan Feb Mar
+     Quarter 2 :- Apr May Jun
+     Quarter 3 :- Jul Aug Sep
+     Quarter 4 :- Oct Nov Dec
+
+4)Want Quarter from the Column:-
+     Formula :- Year = YEAR('TCS NS'[Date].[Date]) 
